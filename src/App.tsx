4f274fd43a997fbcc94c04e872e857e8d4e4dbd9
likithkun <likith1238@gmail>
2024-02-11 00:07:12 +0530

    Built a resposive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
-import { Button, ButtonGroup } from '@chakra-ui/react'
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
-  
-
-  return (<Button colorScheme='blue'>Button</Button>
-  )
+  return (
+    // to create resposive layout, use templateAreas , lg
+    <Grid templateAreas={{
+      base: `"nav"" main"`,
+      lg : `"nav nav" "aside main"`
+    }}>
+      <GridItem bg="tomato" gridArea="nav">
+        nav
+      </GridItem>
+      {/*show above lg renders the aside component only when the screen size is larger than  or equal to lg */}
+      <Show above="lg">
+        <GridItem bg="papayawhip" gridArea="aside">
+          aside
+        </GridItem>
+      </Show>
+      <GridItem bg="lightgray" gridArea="main">
+        main
+      </GridItem>
+    </Grid>
+  );
 }
 
-export default App
+export default App;
